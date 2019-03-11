618ce83cdad3f22b599047a572782750d9b2a5f8
ThijsOnGitHup <47325246+ThijsOnGitHup@users.noreply.github.com>
2019-03-11 11:22:37 +0100

    go

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-.
+var joinButton=document.getElementById("joinGame.but")
+var playerName=document.getElementById("playerName.inv")
+var playerCode=document.getElementById("playerCode.inv")
+
+joinButton.addEventListener("click",function () {
+    window.location.href="./Player Game/Player Game.html?id=123"
+})
